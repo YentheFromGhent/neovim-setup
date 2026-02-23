28130cbfbd5717670a304b31aabff2f09f881b26
yenthe <yenthefromghent@gmail.com>
2026-02-23 23:40:24 +0100

    finished new config

--- init.lua
+++ init.lua
@@ -61,6 +61,7 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>ss", ":w<CR>", {desc = "Write file"})
 vim.keymap.set("n", "<leader>sq", ":wq<CR>", { desc = "Write and quit file" })
+vim.keymap.set("n", "<leader>fq", ":q!<CR>", { desc = "Force quit" })
 vim.keymap.set("n", "<leader>cs", ":noh<CR>", { desc = "Clear search highlighting" })
 
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
@@ -74,6 +75,46 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 
+vim.keymap.set('n', '<Space>', '<Nop>')
+vim.keymap.set('v', '<Space>', '<Nop>')
+
+
+-- Plugins (using nvim-pack)
+vim.pack.add({
+    "https://github.com/ibhagwan/fzf-lua.git",
+    "https://github.com/nvim-mini/mini.nvim.git",
+    "https://github.com/lewis6991/gitsigns.nvim.git",
+    "https://www.github.com/nvim-tree/nvim-tree.lua",
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+	},
+    "https://www.github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+    {
+		src = "https://github.com/saghen/blink.cmp",
+		version = vim.version.range("1.*"),
+	},
+	"https://github.com/L3MON4D3/LuaSnip",
+    "https://github.com/ThePrimeagen/harpoon.git",
+    "https://github.com/nvim-lua/plenary.nvim.git"
+})
+
+local function packadd(name)
+    vim.cmd("packadd " .. name)
+end
+packadd("fzf-lua")
+packadd("mini.nvim")
+packadd("gitsigns.nvim")
+packadd("nvim-treesitter")
+packadd("nvim-lspconfig")
+packadd("mason.nvim")
+packadd("blink.cmp")
+packadd("LuaSnip")
+packadd("harpoon")
+packadd("plenary.nvim")
+
 -- Lua line
 local lualine_theme = require"lualine.themes.iceberg_dark"
 
@@ -81,3 +122,182 @@ require('lualine').setup{
     options = { theme = lualine_theme },
 }
 
+-- fzf 
+
+require("fzf-lua").setup({})
+
+vim.keymap.set("n", "<leader>ff", function()
+	require("fzf-lua").files()
+end, { desc = "FZF Files" })
+vim.keymap.set("n", "<leader>fg", function()
+	require("fzf-lua").live_grep()
+end, { desc = "FZF Live Grep" })
+vim.keymap.set("n", "<leader>fb", function()
+	require("fzf-lua").buffers()
+end, { desc = "FZF Buffers" })
+vim.keymap.set("n", "<leader>fh", function()
+	require("fzf-lua").help_tags()
+end, { desc = "FZF Help Tags" })
+vim.keymap.set("n", "<leader>fx", function()
+	require("fzf-lua").diagnostics_document()
+end, { desc = "FZF Diagnostics Document" })
+vim.keymap.set("n", "<leader>fX", function()
+	require("fzf-lua").diagnostics_workspace()
+end, { desc = "FZF Diagnostics Workspace" })
+
+-- Mini.nvim
+require("mini.icons").setup({})
+require("mini.pairs").setup({})
+
+-- Treesitter
+local setup_treesitter = function()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup({})
+    local ensure_installed = {
+        "vim", 
+        "vimdoc",
+        "c",
+        "cpp",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "bash",
+    }
+
+    local config = require("nvim-treesitter.config")
+
+	local already_installed = config.get_installed()
+	local parsers_to_install = {}
+
+	for _, parser in ipairs(ensure_installed) do
+		if not vim.tbl_contains(already_installed, parser) then
+			table.insert(parsers_to_install, parser)
+		end
+	end
+
+	if #parsers_to_install > 0 then
+		treesitter.install(parsers_to_install)
+	end
+
+	local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		callback = function(args)
+			if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+				vim.treesitter.start(args.buf)
+			end
+		end,
+	})
+end
+
+setup_treesitter()
+
+-- Lspconfig
+
+require("mason").setup({})
+
+local diagnostic_signs = {
+	Error = " ",
+	Warn = " ",
+	Hint = "",
+	Info = "",
+}
+
+vim.diagnostic.config({
+	virtual_text = { prefix = "●", spacing = 4 },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+			[vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+			[vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+			[vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+		},
+	},
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+		focusable = false,
+		style = "minimal",
+	},
+})
+
+do
+	local orig = vim.lsp.util.open_floating_preview
+	function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+		opts = opts or {}
+		opts.border = opts.border or "rounded"
+		return orig(contents, syntax, opts, ...)
+	end
+end
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			telemetry = { enable = false },
+		},
+	},
+})
+vim.lsp.config("bashls", {})
+vim.lsp.config("ts_ls", {})
+vim.lsp.config("clangd", {})
+
+vim.lsp.enable({
+    "lua_ls",
+    "bashls",
+    "ts_ls",
+    "clangd"
+})
+
+-- Blink 
+
+require("blink.cmp").setup({
+	keymap = {
+		preset = "none",
+		["<A-Space>"] = { "show", "hide" },
+		["<CR>"] = { "accept", "fallback" },
+		["<C-j>"] = { "select_next", "fallback" },
+		["<C-k>"] = { "select_prev", "fallback" },
+		["<Tab>"] = { "snippet_forward", "fallback" },
+		["<S-Tab>"] = { "snippet_backward", "fallback" },
+	},
+	appearance = { nerd_font_variant = "mono" },
+	completion = { menu = { auto_show = true } },
+	sources = { default = { "lsp", "path", "buffer", "snippets" } },
+	snippets = {
+		expand = function(snippet)
+			require("luasnip").lsp_expand(snippet)
+		end,
+	},
+
+	fuzzy = {
+		implementation = "prefer_rust",
+		prebuilt_binaries = { download = true },
+	},
+})
+
+vim.lsp.config["*"] = {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
+}
+
+-- Harpoon
+
+local harpoon_mark = require("harpoon.mark")
+local harpoon_ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<C-a>", function() harpoon_mark.add_file() end)
+vim.keymap.set("n", "<C-l>", function() harpoon_ui.toggle_quick_menu() end)
+
+vim.keymap.set("n", "<C-1>", function() harpoon_ui.nav_file(1) end)
+vim.keymap.set("n", "<C-2>", function() harpoon_ui.nav_file(2) end)
+vim.keymap.set("n", "<C-3>", function() harpoon_ui.nav_file(3) end)
+vim.keymap.set("n", "<C-4>", function() harpoon_ui.nav_file(4) end)
